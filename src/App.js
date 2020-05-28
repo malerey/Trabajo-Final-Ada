4548d401a8ad2e8eff55a566e61ca27c40bb0124
fpezcara <fpezcara@gmail.com>
2020-05-28 21:17:24 +0100

    cambié lugar router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import BarraDeNav from "./components/BarraDeNav";
-import Main from "./components/Main";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import HomeComponent from "./components/HomeComponent";
+import VideoComponent from "./components/VideoComponent";
+import TvComponent from "./components/TvComponent";
+import SearchComponent from "./components/SearchComponent";
+import DisplayCardsFull from "./components/DisplayCardsFull";
 
 const App = () => {
   return (
-    <div className="App">
-      <BarraDeNav />
-      <Main />
+    <div>
+    <Router>
+      <BarraDeNav>      
+      <Switch>
+        <Route exact path="/" component={HomeComponent}></Route>
+        {/* <Route exact path="/home" component={HomeComponent}></Route> */}
+        <Route path="/video" component={VideoComponent}></Route>
+        <Route path="/tv" component={TvComponent}></Route>
+        <Route path="/search" component={SearchComponent}></Route>
+        <Route path="/displaycardsfull" component={DisplayCardsFull}></Route>
+      </Switch>
+          
+      </BarraDeNav>
+      </Router>
+
     </div>
   );
 };
--- src/components/Main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HomeComponent from './HomeComponent'
-
-const Main = () => {
-    return <HomeComponent />
-}
-
-export default Main;
